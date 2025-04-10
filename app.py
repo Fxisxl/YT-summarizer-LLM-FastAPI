256b8fe10336e886b4ee84157f2d296db3ab01ba
Faisal <shkfs9058@gmail.com>
2025-04-10 21:17:22 +0530

    made some minor changes

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from models import generate_YT_summary, chatbot_with_history
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins (you can specify a list of allowed domains)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
 class YTRequest(BaseModel):
-    ytlink: str
+    user_query: str
     session: str
+    mode : str
 
 class ChatRequest(BaseModel):
     user_query: str
     session: str
+    mode : str
 
 @app.post("/summarize")
 def summarize_video(request: YTRequest):
     """Endpoint to summarize a YouTube video transcript and store it."""
-    print("entered the route")
-    summary =  generate_YT_summary(request.ytlink, request.session)
+    print("entered the route : " , request.mode)
+    summary =  generate_YT_summary(request.user_query, request.session)
     print("Returning response")
     return {"summary": summary}
 
 @app.post("/chat")
 def chat_with_bot(request: ChatRequest):
     """Endpoint to interact with the chatbot using history-aware retrieval."""
-    answer, history = chatbot_with_history(request.user_query, request.session)
+    print("entered the CHAT route " , request.mode)
+    answer ,history = chatbot_with_history(request.user_query, request.session)
+    print("Returning response")
+    print(answer)
     return {"answer": answer}
 
 
@@ -31,3 +46,4 @@ def chat_with_bot(request: ChatRequest):
 if __name__=="__main__":
     uvicorn.run(app,host="localhost",port=8000)
 
+#uvicorn app:app --host 0.0.0.0 --port 8000 --reload
